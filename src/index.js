2e76c239fd8ed6ca7de45d5f9cdd8a4eb175bc2c
Laurenh89 <Laurenvharris.89@gmail.com>
2025-01-31 13:49:39 +0000

    Improved the User experience and removed the console logs

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 function displayPoem(response) {
-  console.log("poem generated");
   new Typewriter("#poem", {
     strings: response.data.answer,
     autoStart: true,
@@ -18,9 +17,9 @@ function generatePoem(event) {
   let prompt = `User instructions: Generate an English poem about ${instructionsInput.value}`;
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-  console.log("Generating poem");
-  console.log(`Prompt: ${prompt}`);
-  console.log(`Context: ${context}`);
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating">Generating a poem for you about ${instructionsInput.value}</div>`;
 
   axios.get(apiURL).then(displayPoem);
 }
